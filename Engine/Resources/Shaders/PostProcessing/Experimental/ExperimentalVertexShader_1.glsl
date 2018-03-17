cdcec0f35980ed5f82e866ebee2b7cf0e4c90b2d
esilvert <ervan.silvert@mailoo.org>
2018-03-17 04:05:26 +0100

    Van playing with post process

--- Engine/Resources/Shaders/PostProcessing/Experimental/ExperimentalVertexShader_1.glsl
+++ Engine/Resources/Shaders/PostProcessing/Experimental/ExperimentalVertexShader_1.glsl
@@ -6,8 +6,29 @@ layout(location = 0) in vec2 vertexCoord;
 // Out
 out vec2 textureUV;
 
+uniform float RAND_MAX = 43758.5453f;
+
+// Simple pseudo-random
+float rand(vec2 v2_seed)
+{
+    return fract(sin(dot(v2_seed.xy ,vec2(12.9898,78.233))) * RAND_MAX);
+}
+
+// Does nothing
+void noop()
+{
+    // Position
+    gl_Position = vec4(vertexCoord, 0.0, 1.0);
+
+    // Texture UV
+    textureUV   = (vertexCoord + 1.0) / 2.0;
+}
+
 void main(void)
 {
-  gl_Position = vec4(vertexCoord, 0.0, 1.0);
-  textureUV   = (vertexCoord + 1.0) / 2.0;
+    // Position
+    gl_Position = vec4(0,0,0, 1.0);
+    
+    // Texture UV
+    textureUV   = (vertexCoord + 1.0) / 2.0;
 }
